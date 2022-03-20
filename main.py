2c2ddf8157350d0753567ffa4c6cb5e29a3b1d0a
jakaco <83722848+jakaco@users.noreply.github.com>
2022-03-20 17:23:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,66 +11,63 @@ from datetime import datetime
 
 from pynput import keyboard
 
-# CONFIG
+fontname   = "Hack-Regular.ttf"
+fontsize   = 16
+width      = 64
+height     = 36
+filename   = "time.gif"
+command    = "asusctl anime pixel-gif -p time.gif"
+textcolour = (255, 255, 255)
+timeformat = "%H:%M"
+mode       = "RGB"
+keybind    = 269025089
 
-fontname   = "Hack-Regular.ttf" # Font (must be in same directory)
-fontsize   = 50  # Font size
-width      = 256 # Image size
-height     = 256
-filename   = "time.png" # Filename of imge output
-command    = "asusctl anime image -p time.png -s 2.25 -a 0.65 -x -1.5 -y 1.5" # Command to be run every minute. Make sure the -p is the same as the filename. Put all your fine tweaking here. 
-textcolour = (255, 255, 255) # Text colour
-timeformat = "%H:%M" # TODO: if you add %S it will still update every minute not second
-mode       = "RGB" 
-keybind    = 269025089 # XF86Launch3
 
-# END CONFIG
-
-font = ImageFont.truetype(fontname, fontsize) # Register font
+font = ImageFont.truetype(fontname, fontsize)
 W,H = (width, height)
 
 active = True
 
 def main():
-	now = datetime.now() # Get the current time
-	strtime = now.strftime(timeformat) # Get time sstirng
+    now = datetime.now()
+    strtime = now.strftime(timeformat)
 
-	img = Image.new(mode, (width, height)) # Create new image
-	draw = ImageDraw.Draw(img)
-	
-	w,h = draw.textsize(strtime, font=font) # Get size of text so we can center it
-	draw.text(((W-w)/2, (H-h)/2), strtime, textcolour,font=font) # Draw the text in the center
-	
-	img.save(filename)
+    img = Image.new(mode, (width, height))
+    draw = ImageDraw.Draw(img)
 
-	system(command) # Update matrix
+    w,h = draw.textsize(strtime, font=font)
+    draw.text(((W-w)/2, (H-h)/1.1), strtime, textcolour,font=font)
+
+    img.save(filename)
+
+    system(command)
 
 def cleanup(signal, frame):
-	system("asusctl anime -e false") # Turn off matrix on sigint
-	sys.exit(0)
+    system("asusctl anime -e false")
+    sys.exit(0)
 
 def on_press(key):
-	try: # Get keycode
-		vk = key.vk
-	except AttributeError:
-		vk = key.value.vk
-	if vk == keybind:
-		global active
-		active = not active # Invert active state
-		if active:
-			system("asusctl anime -e true") # Enable display
-			main() # Update display
-		else:
-			system("asusctl anime -e false") # Disable display
+    try:
+        vk = key.vk
+    except AttributeError:
+        vk = key.value.vk
+    if vk == keybind:
+        global active
+        active = not active
+        if active:
+            system("asusctl anime -e true")
+            main()
+        else:
+            system("asusctl anime -e false")
 
 if __name__ == "__main__":
-	system("asusctl anime -e true") # Enable display
-	signal.signal(signal.SIGINT, cleanup) # Register sigint handler
-	listener = keyboard.Listener( # Register keyboard listener
-		on_press=on_press
-	)
-	listener.start()
-	while True:
-		if active :
-			main()
-		time.sleep(60 - time.time() % 60) # Run at the start of every second
+    system("asusctl anime -e true")
+    signal.signal(signal.SIGINT, cleanup)
+    listener = keyboard.Listener(
+        on_press=on_press
+    )
+    listener.start()
+    while True:
+        if active :
+            main()
+        time.sleep(60 - time.time() % 60)
